7aa7a29fc8104b97647cafd8709b20bc217e1e93
Glenn DSouza <glenndsouza@WLAN8-MBP.local>
2020-01-06 10:19:02 -0500

    Updates to IDH time-series visualization

--- idh/functions.py
+++ idh/functions.py
@@ -1,6 +1,14 @@
 import pandas as pd
 from .models import ddo_hourly
 
+from bokeh.plotting import figure
+from bokeh.embed import components
+from bokeh.models import HoverTool
+from bokeh.layouts import row
+
+from datetime import timedelta
+
+
 def import_records():
     df = pd.read_csv('test_data.csv')
     print(df.shape == df.isna().shape and df.shape == df.isnull().shape)
@@ -14,3 +22,19 @@ def import_records():
             if temp_columns[n] != 'hDate' : the_total += j
         temp_dict['hTotal'] = the_total
         ddo_hourly.objects.create(**temp_dict).save()
+
+
+def bokeh_render_hours(q):
+    #This functions takes in a queryset and returns a Bokeh Graph
+    plot = figure(x_axis_label='Date Range', y_axis_label='Individual Donations', x_axis_type='datetime')
+    
+    x = [i.hDate for i in q]
+    y = [i.hTotal for i in q]
+    c = ['5A9D43' if i.hTotal < 100 else 'red' for i in q]
+    
+    # plot.circle([1,10,35,50],[1,2,3,4],size=20, color="blue")
+    # plot.line(x=x,y=y, color='red')
+    plot.vbar(x=x,top=y, width=timedelta(days=0.75), color=c)
+    for i in q:
+        print(vars(i))
+    return row(plot,sizing_mode='scale_both')
--- idh/views.py
+++ idh/views.py
@@ -4,9 +4,14 @@ from bokeh.plotting import figure
 from bokeh.embed import components
 from bokeh.models import HoverTool
 
+from bokeh.layouts import row   
+
 from .models import ddo_hourly
 
 from datetime import datetime
+from .functions import bokeh_render_hours
+
+
 
 # Create your views here.
 def idh(request):
@@ -27,39 +32,45 @@ def idh(request):
     if not flag:
         start_date=datetime.now().replace(day=1)
         end_date = datetime.now()
-
+ 
     ### Step 3 : Crunch the start_date and end_date to strings for Django Date Range + html output
     start_date = datetime.strftime(start_date,'%Y-%m-%d')
     end_date = datetime.strftime(end_date,'%Y-%m-%d')
 
 
-# BOKEH EXPERIMENT#############################################################################
-    # plot=figure()
-    # plot.circle(['h01','h02','h03'],[2,3,5],size=10,color="blue")
-    # (script, div) = components(plot)
-    d = ddo_hourly.objects.filter(hDate__range=[start_date , end_date])
-    fruits = [str(x.hDate) for x in d]
-    counts = [x.hTotal for x in d]
-    # fruits = ['Apples', 'Pears', 'Nectarines', 'Plums', 'Grapes', 'Strawberries']
-    # counts = [5, 3, 4, 2, 4, 6]
-    p1 = figure(x_range=fruits, plot_width=1000, title='')
-    # p.line([i for i,x in enumerate(d)],[y.hTotal for y in d], line_width=1, color='red')
-    p1.vbar(x=fruits, top=counts, width=0.75)
-    p1.xgrid.grid_line_color = None
-    p1.y_range.start = 0
-    p1.xaxis.major_label_orientation = "vertical"
-    (script1, div1) = components(p1)
 
-# BOKEH EXPERIMENT#############################################################################
+# # BOKEH EXPERIMENT#############################################################################
+#     d = ddo_hourly.objects.filter(hDate__range=[start_date , end_date])
+#     fruits = [str(x.hDate) for x in d]
+#     counts = [x.hTotal for x in d]
+#     p1 = figure(x_range=fruits, plot_width=1000, title='')
+#     # p.line([i for i,x in enumerate(d)],[y.hTotal for y in d], line_width=1, color='red')
+#     p1.vbar(x=fruits, top=counts, width=0.75)
+#     p1.xgrid.grid_line_color = None
+#     p1.y_range.start = 0
+#     p1.xaxis.major_label_orientation = "vertical"
+#     (script1, div1) = components(p1)
+# # BOKEH EXPERIMENT#############################################################################
 
 
     # RETURN PART
 
-    message_text = str(flag)
+    d = ddo_hourly.objects.filter(hDate__range=[start_date , end_date])
+    
+    # d is now a QUERYSET of database objects!
+
+
+    
+    
+    
+    # layout = row(bokeh_render_hours(d),bokeh_render_hours(d),bokeh_render_hours(d),sizing_mode='scale_width')
+    layout = bokeh_render_hours(d)
+    script1 , div1 = components(layout)
+    
     return render(request,'idh.html',{
     'script1' : script1,
     'div1' : div1,
-    'message_text' : str(start_date) + '\n' + str(end_date),
+    'message_text' : str(56),
     'start_date' : start_date,
     'end_date' : end_date,
     'max_date' : datetime.strftime(datetime.now(),'%Y-%m-%d')
