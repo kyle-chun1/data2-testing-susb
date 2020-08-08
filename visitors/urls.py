40763c7b4e5a2da982906ee0d4c9157ea234da22
Glenn DSouza <github@glenndata.com>
2020-08-08 16:57:00 -0400

    Made a dynamic view for visiors + some small changes and improvements

--- visitors/urls.py
+++ visitors/urls.py
@@ -5,11 +5,8 @@ from django.urls import path
 from visitors.views import *
 
 urlpatterns = [
-    path('irc/', irc, name='irc'),
-    path('trc/', trc, name='trc'),
-    path('ddo/', ddo, name='ddo'),
-    path('rch/', rch, name='rch'),
-    path('test/', test, name='test'),
+
     path('submit/', submit, name='submit'),
+    path('<slug:location>/', VISITORS, name='VISITORS'),
 
 ]
--- visitors/views.py
+++ visitors/views.py
@@ -86,3 +86,35 @@ def submit(request):  #ASUME LOCATIONS ARE CORRECT
         Visitors(timestamp=timezone.now(),flr_email=flr_email,count=count,location=location).save()
 
     return redirect('/visitors/' + location.lower() + '/')
+
+
+
+
+
+
+def VISITORS(request, location=''):
+    #IF USER IS NOT AUTHENTICATED SEND THEM HOME!
+    if not request.user.is_authenticated:
+        return redirect('HOME')
+    # HARDCODED STUFF - LOCATION
+    if location.strip() == '' or location.upper() not in ['IRC','TRC','RCH','DDO','TEST']:
+        return redirect('HOME')
+    else:
+        location = location.upper()
+    #HARDCODED
+    capacity = {'IRC':70,'TRC':92,'RCH':88,'DDO':999,'TEST':999}[location]
+    current = Visitors.objects.filter(location=location).aggregate(Sum('count'))['count__sum']
+    total_today = Visitors.objects.filter(location=location).filter(timestamp__date=timezone.now()).filter(count__gte=0).aggregate(Sum('count'))['count__sum']
+
+    render_dict = {
+        'capacity': {'IRC':70,'TRC':92,'RCH':88,'DDO':999,'TEST':999}[location],
+        'location' : location,
+        'current' : Visitors.objects.filter(location=location).filter(timestamp__date=timezone.now()).aggregate(Sum('count'))['count__sum'],
+        'total_today' : Visitors.objects.filter(location=location).filter(timestamp__date=timezone.now()).filter(count__gte=0).aggregate(Sum('count'))['count__sum']
+    }
+    print(timezone.now())
+    return render(request, 'visitors/index.html',render_dict)
+
+
+def TESTER2(request, message=''):
+    return redirect('TESTER1', message='YOLOMF')
