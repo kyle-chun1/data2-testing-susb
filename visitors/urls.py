5e857f5e86c1f59cb8c1f924710cb77c4f7df0f5
Glenn DSouza <github@glenndata.com>
2020-08-11 03:30:21 -0400

    experimental capacity chart

--- visitors/urls.py
+++ visitors/urls.py
@@ -7,6 +7,8 @@ from visitors.views import *
 urlpatterns = [
 
     path('submit/', submit, name='submit'),
+    path('capacity/', capacity, name='capacity'),
     path('<slug:location>/', VISITORS, name='VISITORS'),
+
 
 ]
--- visitors/views.py
+++ visitors/views.py
@@ -16,6 +16,13 @@ import pandas as pd
 from visitors.models import Visitors
 from django.utils import timezone
 
+
+from bokeh.plotting import figure
+from bokeh.embed import components
+from bokeh.models import HoverTool, ColumnDataSource, ranges, LabelSet
+
+
+
 def submit(request):  #ASUME LOCATIONS ARE CORRECT
     #IF USER IS NOT AUTHENTICATED SEND THEM HOME!
     if not request.user.is_authenticated:
@@ -91,4 +98,68 @@ def VISITORS(request, location=''):
         'info': info,
     }
 
+    for i in Visitors_here_today:
+        print(useastern(i.timestamp), i.count)
+
     return render(request, 'visitors/index.html',render_dict)
+
+
+
+
+
+
+
+
+
+
+###########################################################################
+# ALL STORES VIEW - 3 Column charset
+def capacity(request):
+
+    try:
+        GET_start = datetime.strptime(request.GET['start'], "%Y-%m-%d")
+        start = useastern().replace(year=GET_start.year, month=GET_start.month, day=GET_start.day)
+
+    except:
+        start = useastern()
+
+    start_date = start.strftime("%Y-%m-%d")  # DONE FOR PARSING IT TO THE HTML START DATE
+
+
+    V = Visitors.objects.filter(timestamp__range=(useastern_start(start),useastern_end(start))).order_by('timestamp')
+
+
+    p = figure(plot_width=1200, plot_height=600,x_axis_type="datetime")
+
+    locations = {'IRC':'green','TRC':'blue','RCH':'red'}
+    capacities = {'IRC':70,'TRC':92,'RCH':88}
+    for location in locations:
+        print([(x.location,x.count) for x in V.filter(location=location)])
+        x = [useastern(x.timestamp) for x in V.filter(location=location)]
+        y = [y.count for y in V.filter(location=location)]
+        running_sum = 0
+        Y = []
+        for i in y:
+            running_sum += i
+            Y.append(running_sum/capacities[location]*100)
+        print("\n\n\n",Y,"\n",y,"\n\n\n\n")
+        p.line(x,Y,color=locations[location],line_width=4,line_alpha=0.5,legend_label=location)
+
+    p.legend.click_policy="hide"
+    p.xaxis.axis_label = 'Time of Day'
+    p.yaxis.axis_label = 'Percentage (%) of Capacity Reached'
+###############RENDER PART
+
+
+    # p.patch([1, 2, 3, 4, 5], [6, 7, 8, 7, 3], alpha=0.5, line_width=2, color="#FF0000")
+    # p.line(x,y, color='#000000')
+
+    allstores_bokeh_script, allstores_bokeh_html = components(p)
+
+    render_dict = {
+        'allstores_bokeh_html':'<h3>GOOGLE</h3>',
+        'allstores_bokeh_script' : allstores_bokeh_script,
+        'allstores_bokeh_html': allstores_bokeh_html,
+        'start_date': start_date,
+    }
+    return render(request,'visitors/capacity.html',render_dict)
