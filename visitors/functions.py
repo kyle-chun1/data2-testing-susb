157b09fda9e105d7d07f6b5f49a0f39f5b469ae6
Glenn DSouza <glennforever@gmail.com>
2021-02-23 21:10:37 -0500

    temporary changes updated location counts

--- visitors/functions.py
+++ visitors/functions.py
@@ -66,7 +66,7 @@ from django.db.models import Sum
 
 def capacity_generate(x):
 
-    capacities = {'IRC':70,'TRC':92,'RCH':88, '700-CABOOSE':35, '700-WAREHOUSE': 19, 'TEST':999, 'DDO':999}
+    capacities = {'IRC':70,'TRC':92,'RCH':88, '700-CABOOSE':35, '700-WAREHOUSE': 19, 'TEST':999, 'DDO':999, '700RNR': 35}
 
     QUERY = Visitors.objects.filter(location=x.location, timestamp__range=(useastern_start(x.timestamp), useastern(x.timestamp))).aggregate(Running_total=Sum('count'))
     x.capacity = QUERY['Running_total'] / capacities[x.location]
