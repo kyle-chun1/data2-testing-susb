716c453580d6963cb0975863b3a41232b4635886
Glenn DSouza <glennforever@gmail.com>
2021-03-21 12:53:15 -0400

    updates structure for pricing model

--- pricing/models.py
+++ pricing/models.py
@@ -3,14 +3,14 @@ from django.db import models
 # Create your models here.
 class Location(models.Model):
     location_choices = [('I','IRC'),('T','TRMC')]
-    location = models.CharField(max_length=1, choices=location_choices)
+    location = models.CharField(max_length=1, choices=location_choices, unique=True)
     text = models.TextField(default='')
     shopify_location_id = models.CharField(max_length=16)
     def __str__(self):
         return self.location
 
 class ProductType(models.Model):
-    product_type = models.CharField(max_length=64)
+    product_type = models.CharField(max_length=64, unique=True)
     def __str__(self):
         return self.product_type
 
@@ -20,12 +20,13 @@ class Product(models.Model):
     classifier_choices = [('U','Unit'),('W','White'),('Y','Yellow'),('R','Red'),('O','Orange'),('B','Blue'),('G','Green'),('L','Lavender')]
     classifier = models.CharField(max_length=1, choices = classifier_choices)
     code = models.CharField(max_length=3, unique=True)
-    shopify_handle = models.CharField(max_length=5)
+    shopify_handle = models.CharField(max_length=5, unique=True)
+    title = models.CharField(max_length=256, default='')
     def __str__(self):
         return self.shopify_handle
 
 class Variant(models.Model):   #PROXY FOR BARCODE / SKU (Can be referenced!)
-    variant = models.CharField(max_length=16)
+    variant = models.CharField(max_length=16, unique=True)
     product = models.ForeignKey(Product, on_delete=models.PROTECT)
     price = models.DecimalField(max_digits=8,decimal_places=2)
     def __str__(self):
@@ -33,7 +34,16 @@ class Variant(models.Model):   #PROXY FOR BARCODE / SKU (Can be referenced!)
 
 class Pricing(models.Model):
     variant = models.ForeignKey(Variant, on_delete=models.PROTECT)
-    staff_id = models.CharField(max_length=16)
+    staff_id = models.CharField(max_length=32)
     timestamp = models.DateTimeField(auto_now_add=True)
+    quantity = models.IntegerField(default=1)
+    print = models.BooleanField(default=False)
+    inventory = models.BooleanField(default=False)
+    deleted = models.BooleanField(default=False)
+    shopify_inventory = models.BooleanField(default=False)
+
+    # IMPORTANT : NEED TO SORT OUT ISSUE WHERE STAFF DELES ITEM AFTER ADDED TO SHOPIFY INVENTORY
+    # IF CONDITION FOR DELETION -> IF shopify_inventory = True, then REMOVE the appropriate quanity (-NEGETIVE)
+
     def __str__(self):
         return self.staff_id
