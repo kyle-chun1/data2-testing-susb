ccc6f994822364930265491f1d41ad859578ec39
Glenn DSouza <glennforever@gmail.com>
2021-04-07 08:23:59 -0400

    temporary demo commit

--- qbgiftcard/views.py
+++ qbgiftcard/views.py
@@ -2,12 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+from qbgiftcard.models import GiftCard
+
+from json import dumps
 
 def qbgiftcardhome(request):
-    return render(request, 'qbgiftcard/lookup.html', {})
+    G = GiftCard.objects.all()
+    customer_dict = []
+    for i in G:
+        customer_dict.append([i.id, i.first_name, i.last_name, i.phone, i.email ])
+    return render(request, 'qbgiftcard/lookup.html', {'customer_dict': dumps(customer_dict)})
+
+
+
 
 def results(request):
-    return HttpResponse('working - results')
+    G = GiftCard.objects.all()
+    customer_dict = []
+    for i in G:
+        customer_dict.append([i.id, i.first_name, i.last_name, i.phone, i.email ])    
+    return render(request, 'qbgiftcard/lookup_temp.html', {'customer_dict': dumps(customer_dict)})
 
 def lookup(request):
     return HttpResponse('working - lookup')
