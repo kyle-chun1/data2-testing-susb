93926e8d1262786930a087e5e0b564ef84c9e050
Glenn DSouza <glennforever@gmail.com>
2021-04-09 10:56:50 -0400

    complete pre giftcard portal

--- qbgiftcard/views.py
+++ qbgiftcard/views.py
@@ -6,7 +6,11 @@ from django.shortcuts import redirect
 from qbgiftcard.models import GiftCard
 
 from qbgiftcard.models import *
+
 from json import dumps
+
+from datetime import datetime, timedelta
+import pytz
 
 def qbgiftcardhome(request):
     G = GiftCard.objects.all()
@@ -14,16 +18,6 @@ def qbgiftcardhome(request):
     for i in G:
         customer_dict.append([i.id, i.first_name, i.last_name, i.phone, i.email ])
     return render(request, 'qbgiftcard/default1.html', {'customer_dict': dumps(customer_dict)})
-
-
-def results(request):
-    G = GiftCard.objects.all()
-    customer_dict = []
-    for i in G:
-        customer_dict.append([i.id, i.first_name, i.last_name, i.phone, i.email])
-
-    return render(request, 'qbgiftcard/lookup1.html', {'customer_dict': dumps(customer_dict)})
-
 
 
 def lookup(request):
@@ -40,6 +34,19 @@ def lookup(request):
 
 
 
+
+def results(request):
+    if not request.user.is_authenticated:
+        return redirect('HOME')
+    try:
+        staff_id = str(request.user.email).split('@fingerlakesreuse.org')[0]
+        last_record = AccessLog.objects.filter(staff_id=staff_id).reverse().order_by('timestamp')[0]
+        gift_card_code = GiftCard.objects.get(id=last_record.giftcard_id).giftcard
+        gift_card_code = gift_card_code.replace(' ','')
+
+    except:
+        return redirect('qbgiftcard:qbgiftcardhome')
+    return render(request, 'qbgiftcard/lookup1.html', {'gift_card_code': gift_card_code})
 
 
 
