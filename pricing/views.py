906ff61bf030353cf3ec2b1ef0467ae4950109b2
Glenn <glennforever@gmail.com>
2022-03-29 00:35:16 -0400

    Visiblity enforced on dynamic pricing portal

--- pricing/views.py
+++ pricing/views.py
@@ -91,9 +91,9 @@ def pricing_portal_legacy(request, location):
     CC = color_wheel_2021(timezone.now())
     CColor = color_reference[CC]
 
-    products_white = Product.objects.filter(location=Location.objects.get(location=LOCATION), classifier='W').values('shopify_handle','title')
-    products_color = Product.objects.filter(location=Location.objects.get(location=LOCATION), classifier=CC).values('shopify_handle','title')
-    products_unit = Product.objects.filter(location=Location.objects.get(location=LOCATION), classifier='U').values('shopify_handle','title')
+    products_white = Product.objects.filter(visible=True, location=Location.objects.get(location=LOCATION), classifier='W').values('shopify_handle','title')
+    products_color = Product.objects.filter(visible=True, location=Location.objects.get(location=LOCATION), classifier=CC).values('shopify_handle','title')
+    products_unit = Product.objects.filter(visible=True, location=Location.objects.get(location=LOCATION), classifier='U').values('shopify_handle','title')
 
     V = Variant.objects.filter(product__location=Location.objects.get(location=LOCATION), product__classifier='U', visible=True).values('variant','title','price').order_by('title')
 
